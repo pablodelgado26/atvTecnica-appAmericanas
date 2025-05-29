c3eb9086398664fe5d3a397895bf8f6a57da46b6
Pablo Delgado <pablo.j.abreu@aluno.senai.br>
2025-05-29 14:31:33 -0300

    rafactor: redoing view centering on the basket page

--- app/_layout.js
+++ app/_layout.js
@@ -1,6 +1,7 @@
 import { Tabs, Stack } from "expo-router";
 import Entypo from '@expo/vector-icons/Entypo';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 
 export default function Layout() {
     return (
@@ -31,6 +32,16 @@ export default function Layout() {
                     tabBarLabel: "Categorias", // Adiciona o nome abaixo do ícone
                     tabBarIcon: ({ color, size }) => (
                         <Entypo name="menu" size={size} color={color} />
+                    ),
+                }}
+            />
+            <Tabs.Screen
+                name="basket"
+                options={{
+                    headerShown: false,
+                    tabBarLabel: "Cesta", // Adiciona o nome abaixo do ícone
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome5 name="shopping-basket" size={size} color={color} />
                     ),
                 }}
             />
--- app/basket.js
+++ app/basket.js
@@ -63,8 +63,8 @@ const styles = StyleSheet.create({
     },
 
     content: {
-        width: '100%',
-        height: '100%',
+        marginBottom: 'auto',
+        marginTop: 'auto',
         alignItems: 'center',
         justifyContent: 'center',
         display: 'flex',
